438294d9c749b8e3ea59a6ac1604f3dbebb1217c
Idan Hahn <idanhahn@gmail.com>
2020-06-08 16:41:48 +0300

    divide to root level blocks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material';
+import { HeaderComponent } from './header/header.component';
+import { RightPanelComponent } from './right-panel/right-panel.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListItemComponent } from './list-item/list-item.component';
+import { CustomersComponent } from './customers/customers.component';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RightPanelComponent,
+    FooterComponent,
+    ListItemComponent,
+    CustomersComponent,
+    LeftPanelComponent
   ],
   imports: [
     BrowserModule,
